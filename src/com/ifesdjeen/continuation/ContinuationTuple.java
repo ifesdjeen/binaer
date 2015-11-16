ac24dbde6da5ddfdb471665169f285f991393ce6
Alex Petrov <oleksandr.petrov@gmail.com>
2015-11-16 22:45:09 +0100

    Cleanup

--- src/com/ifesdjeen/continuation/ContinuationTuple.java
+++ src/com/ifesdjeen/continuation/ContinuationTuple.java
@@ -6,11 +6,11 @@ import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-public class Tuple<PREVIOUS, END> {
+public class ContinuationTuple<PREVIOUS, END> {
   private final BiFunction<PREVIOUS, ByteBuf, END> prev;
   private final Predicate<PREVIOUS>                predicate;
 
-  public Tuple(Predicate<PREVIOUS> predicate, BiFunction<PREVIOUS, ByteBuf, END> prev) {
+  public ContinuationTuple(Predicate<PREVIOUS> predicate, BiFunction<PREVIOUS, ByteBuf, END> prev) {
     this.prev = prev;
     this.predicate = predicate;
   }
