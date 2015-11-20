5f9a8180d29cb7a9ac0432e834e20813e87ae291
Alex Petrov <oleksandr.petrov@gmail.com>
2015-11-20 15:22:12 +0100

    Remove unneeded parts

--- src/com/ifesdjeen/continuation/ContinuationImpl.java
+++ src/com/ifesdjeen/continuation/ContinuationImpl.java
@@ -1,108 +1,94 @@
 package com.ifesdjeen.continuation;
 
 import io.netty.buffer.ByteBuf;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
-public class ContinuationImpl<CURRENT> implements Continuation<CURRENT> {
+public class ContinuationImpl<PREVIOUS, CURRENT> implements Continuation<PREVIOUS, CURRENT> {
 
-  private final Function<ByteBuf, CURRENT> parentContinuation;
+  private final Function<PREVIOUS, Function<ByteBuf, CURRENT>> parentContinuation;
 
-  public ContinuationImpl(Function<ByteBuf, CURRENT> parent) {
-    this.parentContinuation = parent;
+  public ContinuationImpl(Function<PREVIOUS, Function<ByteBuf, CURRENT>> parentContinuation) {
+    this.parentContinuation = parentContinuation;
   }
 
+
   @Override
-  public <NEXT> Continuation<NEXT> readByte(BiFunction<CURRENT, Byte, NEXT> continuation) {
-    return new ContinuationImpl<>((byteBuf) -> {
-      CURRENT current = parentContinuation.apply(byteBuf);
-      byte b = byteBuf.readByte();
-      return continuation.apply(current, b);
+  public <T> Continuation<PREVIOUS, T> readByte(BiFunction<CURRENT, Byte, T> continuation) {
+    return new ContinuationImpl<>((previous) -> {
+      Function<ByteBuf, CURRENT> fn = parentContinuation.apply(previous);
+      return (byteBuffer) -> {
+        CURRENT c = fn.apply(byteBuffer);
+        return continuation.apply(c, byteBuffer.readByte());
+      };
     });
   }
 
   @Override
-  public <T> Continuation<T> readInt(BiFunction<CURRENT, Integer, T> continuation) {
-    return new ContinuationImpl<>((byteBuf) -> {
-      CURRENT current = parentContinuation.apply(byteBuf);
-      int b = byteBuf.readInt();
-      return continuation.apply(current, b);
+  public <T> Continuation<PREVIOUS, T> readInt(BiFunction<CURRENT, Integer, T> continuation) {
+    return new ContinuationImpl<>((previous) -> {
+      Function<ByteBuf, CURRENT> fn = parentContinuation.apply(previous);
+      return (byteBuffer) -> {
+        CURRENT c = fn.apply(byteBuffer);
+        return continuation.apply(c, byteBuffer.readInt());
+      };
     });
   }
 
   @Override
-  public <T> Continuation<T> readLong(BiFunction<CURRENT, Long, T> continuation) {
-    return new ContinuationImpl<>((byteBuf) -> {
-      CURRENT current = parentContinuation.apply(byteBuf);
-      long b = byteBuf.readLong();
-      return continuation.apply(current, b);
+  public <T> Continuation<PREVIOUS, T> readLong(BiFunction<CURRENT, Long, T> continuation) {
+    return new ContinuationImpl<>((previous) -> {
+      Function<ByteBuf, CURRENT> fn = parentContinuation.apply(previous);
+      return (byteBuffer) -> {
+        CURRENT c = fn.apply(byteBuffer);
+        return continuation.apply(c, byteBuffer.readLong());
+      };
     });
   }
 
   @Override
-  public <T> Continuation<T> readString(BiFunction<CURRENT, String, T> continuation, Integer length) {
-    return new ContinuationImpl<>((byteBuf) -> {
-      CURRENT current = parentContinuation.apply(byteBuf);
-      byte[] bytes = new byte[length];
-      byteBuf.readBytes(bytes);
-      return continuation.apply(current, new String(bytes));
+  public <T> Continuation<PREVIOUS, T> readString(BiFunction<CURRENT, String, T> continuation, Integer length) {
+    throw new NotImplementedException();
+  }
+
+  @Override
+  public <T> Continuation<PREVIOUS, T> readString(BiFunction<CURRENT, String, T> continuation,
+                                                  Function<CURRENT, Integer> length) {
+    throw new NotImplementedException();
+  }
+
+  @Override
+  public <T> Continuation<PREVIOUS, T> branch(Predicate<CURRENT> predicate,
+                                              Continuation<CURRENT, T> continuation,
+                                              Predicate<CURRENT> predicate2,
+                                              Continuation<CURRENT, T> continuation2) {
+    return new ContinuationImpl<>((previous) -> {
+      Function<ByteBuf, CURRENT> fn = parentContinuation.apply(previous);
+      return (byteBuf) -> {
+        CURRENT c = fn.apply(byteBuf);
+        if (predicate.test(c)) {
+          return continuation.toFn(() -> c)
+                             .apply(c, byteBuf); // TODO: avoid dereferencing every time
+        } else if (predicate2.test(c)) {
+          return continuation2.toFn(() -> c)
+                              .apply(c, byteBuf);
+        } else {
+          throw new RuntimeException("No matching protocol clauses");
+        }
+      };
     });
   }
 
   @Override
-  public <T> Continuation<T> readString(BiFunction<CURRENT, String, T> continuation, Function<CURRENT, Integer> length) {
-    return new ContinuationImpl<>((byteBuf) -> {
-      CURRENT current = parentContinuation.apply(byteBuf);
-      byte[] bytes = new byte[length.apply(current)];
-      byteBuf.readBytes(bytes);
-      return continuation.apply(current, new String(bytes));
+  public BiFunction<PREVIOUS, ByteBuf, CURRENT> toFn(Supplier<PREVIOUS> supplier) {
+    return ((previous, byteBuf) -> {
+      // TODO: maybe utilize supplier here?
+      return parentContinuation.apply(previous).apply(byteBuf);
     });
   }
-
-  @Override
-  public <T> Continuation<T> branch(Predicate<CURRENT> predicate, NestedContinuation<CURRENT, T> continuation,
-                                    // TODO: chanage to branch
-                                    Predicate<CURRENT> predicate2, NestedContinuation<CURRENT, T> continuation2) {
-    return new ContinuationImpl<>((byteBuf -> {
-      CURRENT c = parentContinuation.apply(byteBuf);
-      if (predicate.test(c)) {
-        return continuation.toFn().apply(c, byteBuf); // TODO: avoid dereferencing every time
-      } else if (predicate2.test(c)) {
-        return continuation2.toFn().apply(c, byteBuf);
-      } else {
-        throw new RuntimeException("No matching protocol clauses");
-      }
-    }));
-  }
-
-  @Override
-  public Function<ByteBuf, CURRENT> toFn() {
-    return parentContinuation;
-  }
-
-  //  @Override
-  //  public <T> Continuation<List<T>, END> repeat(BiFunction<CURRENT, Integer, T> continuation) {
-  //    return null;
-  //  }
-
-  //  @Override
-  //  public <T> Continuation<T, END> optional(BiFunction<CURRENT, Integer, Optional<T>> optional) {
-  //    return null;
-  //  }
-
-  //
-  //  @Override
-  //  public Function<ByteBuf, END> build() {
-  //    return null;
-  //  }
-
-  //  @Override
-  //  public END apply(ByteBuf byteBuf) {
-  //    return null;
-  //  }
-
-  // TODO: contunue
 
 }
